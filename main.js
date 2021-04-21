0f38749feed4e4779e497b39bca20bf42c7d0592
Tyler <tyler_venetsanos@msn.com>
2021-04-21 09:14:38 -0400

    Completed Assignment

--- main.js
+++ main.js
@@ -2,11 +2,36 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+likerButton()
 
+function likerButton() {
+  let likes = document.getElementsByClassName('like')
+  for (let element of likes) {
+    element.addEventListener('click', () => {
+      const heart = element.getElementsByTagName('span')[0]
+      const modal = document.getElementById('modal')
+      const modalMessage = document.getElementById('modal-message')
 
+      if (heart.textContent === FULL_HEART) {
+        heart.textContent = EMPTY_HEART
+        heart.classList.remove('activated-heart')
+        return EMPTY_HEART
+      }
 
+      mimicServerCall()
+      .then(() => {
+        heart.classList.add('activated-heart')
+        heart.textContent = FULL_HEART
+      })
+      .catch(error => {
+        modal.classList.remove('hidden')
+        modalMessage.textContent = error 
 
+        setTimeout(function(){ modal.classList.add("hidden"); }, 3000);
+      })
+    })
+  }
+}
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
